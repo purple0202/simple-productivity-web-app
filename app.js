31ea9db713eb7ecdbfc1dc90da5b36c9b0779044
Vince Park <vincepark@Vinces-iMac.modem>
2025-11-25 01:56:36 +1100

    js practice - event listener

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("hi");
+// const title = document.getElementById("title")
+const title = document.querySelector("div.hello:first-chiled h1");
+
+// console.dir(title)
+
+// const hellos = document.getElementByCLass
+title.addEventListener("click")
