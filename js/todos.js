ce472104fcbb84fa377af70fb0d178684131a10a
Vince Park <vincepark@Vinces-iMac.modem>
2025-11-30 03:52:47 +1100

    todolist, save arrays at local storage

--- js/todos.js
+++ js/todos.js
@@ -1,6 +1,7 @@
 const todo = document.querySelector("#todo-form");
 const todoInput = todo.querySelector("input");
-const todoList = document.querySelector("todo-list");
+const todoList = document.querySelector("#todo-list");
+let toDos = [];
 // const HIDDEN_CLASSNAME = "hidden"
 
 // if (savedUsername != null){
@@ -30,11 +31,50 @@ function showTodo() {
     }
 }
 
+function deleteTodo(event){
+    console.log(event.target.parentNode.innerText);
+    todoList.removeChild(event.target.parentNode);
+}
+
+function paintToDo(newTodo){
+    // console.log("i will paint", newTodo);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo;
+    console.log(li);
+    todoList.appendChild(li);
+
+}
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     console.log(todoInput.value);
     const newTodo = todoInput.value;
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
     todoInput.value = "";
+    paintToDo(newTodo);
+    saveToDos();
 }
 
 todo.addEventListener("submit", handleToDoSubmit);
+
+const savedItems = localStorage.getItem("todos");
+
+if(savedItems !== null){
+    const parsedToDos = JSON.parse(savedItems);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
